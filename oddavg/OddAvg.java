43da54cca8cc33182b6f68254bc0e230d60b4145
Adam Gyulavari <gyulavari.adam@gmail.com>
2017-04-07 10:51:24 +0200

    add test description for oddavg ex

--- oddavg/OddAvg.java
+++ oddavg/OddAvg.java
@@ -2,5 +2,6 @@ public class OddAvg {
   public static void main(String[] args) {
     // Create a function that takes a list of numbers as parameter
     // and returns the average value of the odd numbers in the list
+    // Create basic unit tests for it with at least 3 different test cases
   }
 }
