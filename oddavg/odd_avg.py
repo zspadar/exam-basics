5836140dfb9e27e25f8c29a2a68e2c7f3c4fd215
Adam Gyulavari <gyulavari.adam@gmail.com>
2017-04-12 16:41:28 +0200

    fix(basics): add method name for first ex

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -1,3 +1,3 @@
-# Create a function that takes a list of numbers as parameter
+# Create a function called `odd_average` that takes a list of numbers as parameter
 # and returns the average value of the odd numbers in the list
 # Create basic unit tests for it with at least 3 different test cases
