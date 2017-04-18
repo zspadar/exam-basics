7b9e81164af59669ff8382f815395107516938a0
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-18 09:47:51 +0200

    (refactor) test folders path

--- OddAvarage/src/Test.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by zsuzsanna.padar on 2017. 04. 18..
- */
-public class Test {
-}
---
+++ OddAvarage/test/OddAvgTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by zsuzsanna.padar on 2017. 04. 18..
+ */
+public class OddAvgTest {
+
+
+}
