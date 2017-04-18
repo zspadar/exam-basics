e4a0fc15b6ec660e12099ed6c2acc56cd35ec660
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-18 11:32:14 +0200

    (add) first two conditions to statements

--- copy/src/Copy.java
+++ copy/src/Copy.java
@@ -1,5 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 /**
  * Created by zsuzsanna.padar on 2017. 04. 18..
  */
 public class Copy {
+
+  public static void main(String[] args) throws IOException {
+    if (args[0] == null) {
+      System.out.println("copy [source] [destination]");
+    } else if (args[1] == null) {
+      System.out.println("No destination provided");
+    } else {
+      
+    }
+  }
 }
