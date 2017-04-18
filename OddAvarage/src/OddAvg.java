d6ee44aba47423bcc8a5e4e5309adc384f2c5340
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-18 10:06:11 +0200

    (test) some test first phase

--- OddAvarage/src/OddAvg.java
+++ OddAvarage/src/OddAvg.java
@@ -9,21 +9,17 @@ public class OddAvg {
     List<Integer> numbers = new ArrayList<>();
   }
 
-    public static double oddAvergae(List<Integer> numbers) {
-      double oddAvg = 0.;
-      int oddSum = 0;
-      int counter = 0;
-      for(int i = 0; i < numbers.size(); i ++) {
-        if(numbers.get(i) % 2 != 0) {
-          oddSum += numbers.get(i);
-          counter++;
-        }
-        oddAvg += oddSum / counter;
+  public static double oddAvergae(List<Integer> numbers) {
+    double oddAvg = 0.;
+    int oddSum = 0;
+    int counter = 0;
+    for(int i = 0; i < numbers.size(); i ++) {
+      if(numbers.get(i) % 2 != 0) {
+        oddSum += numbers.get(i);
+        counter++;
       }
-      return oddAvg;
+      oddAvg += oddSum / counter;
     }
-    // Create a function called `oddAverage` that takes a list of numbers as parameter
-    // and returns the average value of the odd numbers in the list
-    // Create basic unit tests for it with at least 3 different Test cases
-
+    return oddAvg;
+  }
 }
--- OddAvarage/test/OddAvgTest.java
+++ OddAvarage/test/OddAvgTest.java
@@ -1,9 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 import static org.junit.Assert.*;
+
 
 /**
  * Created by zsuzsanna.padar on 2017. 04. 18..
  */
 public class OddAvgTest {
+  @Before
+  public void before() {
+    System.out.println("Before");
+  }
+  @Test
+  public double oddAvergae() throws Exception {
+    List<Integer> numbers = new ArrayList<>();
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+    numbers.add(4);
+    
+
+    assertEquals(2.0, oddAvergae(numbers));
+
+  }
 
 
 }
