afb0ea5f6dd4a2cb3c65607dc54abd9ceac7b0c2
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-18 09:42:45 +0200

    (add) refactored test folder to OddAvarage exercise

--- OddAvarage/src/OddAvg.java
+++ OddAvarage/src/OddAvg.java
@@ -8,7 +8,7 @@ public class OddAvg {
   public static void main(String[] args) {
     List<Integer> numbers = new ArrayList<>();
   }
-  
+
     public static double oddAvergae(List<Integer> numbers) {
       double oddAvg = 0.;
       int oddSum = 0;
@@ -24,6 +24,6 @@ public class OddAvg {
     }
     // Create a function called `oddAverage` that takes a list of numbers as parameter
     // and returns the average value of the odd numbers in the list
-    // Create basic unit tests for it with at least 3 different test cases
+    // Create basic unit tests for it with at least 3 different Test cases
 
 }
