51328beb52248bf03ef12bb151244bbcab4d7c10
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-18 11:02:00 +0200

    (add) constructor and createDeck method for Deck java class

--- OddAvarage/test/OddAvgTest.java
+++ OddAvarage/test/OddAvgTest.java
@@ -14,15 +14,33 @@ public class OddAvgTest {
   public void before() {
     System.out.println("Before");
   }
+//  @Test
+//  public void oddAverage() throws Exception {
+//    ArrayList<Integer> nums = new ArrayList<>();
+//    nums.add(1);
+//    nums.add(2);
+//    nums.add(3);
+//    nums.add(4);
+//    nums.add(5);
+//    assertEquals(3.0, OddAvg.oddAvergae(nums), 0);
+//  }
   @Test
   public void oddAverage() throws Exception {
-    ArrayList<Integer> nums = new ArrayList<>();
-    nums.add(1);
-    nums.add(2);
-    nums.add(3);
-    nums.add(4);
-    nums.add(5);
-    assertEquals(3.0, OddAvg.oddAvergae(nums), 0);
+    ArrayList<Integer> nums1 = new ArrayList<>();
+    nums1.add(1);
+    nums1.add(2);
+    nums1.add(3);
+    nums1.add(4);
+    nums1.add(5);
+    assertEquals(3.0, OddAvg.oddAvergae(nums1), 0);
+    ArrayList<Integer> nums2 = new ArrayList();
+    nums2.add(2);
+    nums2.add(4);
+    nums2.add(1);
+    assertEquals(1.0, OddAvg.oddAvergae(nums2), 0);
+    ArrayList<Integer> nums3 = new ArrayList<>();
+    nums3.add(2);
+    assertEquals(0.0, OddAvg.oddAvergae(nums3), 0);
   }
 
 
--- blackjack/src/Deck.java
+++ blackjack/src/Deck.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -6,5 +8,37 @@ import java.util.List;
 public class Deck {
   private List<Card> cards;
   private int cardsFromDeck;
+
+  public Deck(int cardsFromDeck) {
+    this.cardsFromDeck = cardsFromDeck;
+    this.cards = new ArrayList<>();
+    createDeck();
+  }
+
+  private void createDeck() {
+    List<String> colors = new ArrayList<>();
+    colors.add("Spades");
+    colors.add("Hearts");
+    colors.add("Clubs");
+    colors.add("Diamonds");
+
+    for (int i = 0; i < colors.size(); i++) {
+      cards.add(new Card(colors.get(i), "Ace", 1));
+      cards.add(new Card(colors.get(i), "2", 2));
+      cards.add(new Card(colors.get(i), "3", 3));
+      cards.add(new Card(colors.get(i), "4", 4));
+      cards.add(new Card(colors.get(i), "5", 5));
+      cards.add(new Card(colors.get(i), "6", 6));
+      cards.add(new Card(colors.get(i), "7", 7));
+      cards.add(new Card(colors.get(i), "8", 8));
+      cards.add(new Card(colors.get(i), "9", 9));
+      cards.add(new Card(colors.get(i), "10", 10));
+      cards.add(new Card(colors.get(i), "Jack", 10));
+      cards.add(new Card(colors.get(i), "Queen", 10));
+      cards.add(new Card(colors.get(i), "King", 10));
+    }
+
+    Collections.shuffle(cards);
+  }
 
 }
