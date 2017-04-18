86a2b22c06ffceb3b8376e6ef596ba1b483ee8aa
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-18 11:44:57 +0200

    (add) try catch statements

--- copy/src/Copy.java
+++ copy/src/Copy.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * Created by zsuzsanna.padar on 2017. 04. 18..
@@ -8,12 +9,19 @@ import java.nio.file.Files;
 public class Copy {
 
   public static void main(String[] args) throws IOException {
+    
     if (args.length == 0) {
       System.out.println("copy [source] [destination]");
     } else if (args.length == 1) {
       System.out.println("No destination provided");
     } else {
-
+      File sourceFile = new File(args[0]);
+      File destinationFile = new File(args[1]);
+      try {
+        Files.copy(sourceFile.toPath(), destinationFile.toPath());
+      } catch (IOException e) {
+        System.out.println("Copy error");
+      }
     }
   }
 }
